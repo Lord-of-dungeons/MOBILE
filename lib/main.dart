877275161f15dfcda3cabd75715e300e2ce62b3a
Adrien M <adrien.mld@laposte.net>
2021-12-30 17:57:01 +0100

    📦 NEW: initialisation du système de routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lordofdungeons/screens/login_screen.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lordofdungeons/utils/route_generator.dart';
 import 'package:page_transition/page_transition.dart';
 
 void main() {
@@ -20,13 +21,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'Montserrat'),
       home: AnimatedSplashScreen(
         duration: 3000,
-        splash: "assets/images/logo_start_app.png",
+        splash: 'assets/images/logo_start_app.png',
         nextScreen: LoginScreen(),
         splashTransition: SplashTransition.fadeTransition,
         pageTransitionType: PageTransitionType.fade,
         splashIconSize: double.infinity,
         backgroundColor: Colors.black,
       ),
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
---
+++ lib/utils/route_generator.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:lordofdungeons/screens/login_screen.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 50,
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/images/logo_fond_blanc.png",
+                    width: 250,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Erreur",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Bungee',
+                        fontSize: 18,
+                        color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
+  }
+}
